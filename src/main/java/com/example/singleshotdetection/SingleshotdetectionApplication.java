a72bfa60a34c75ee24ae58588221616a07b76838
sreehariramkumar <sreehariramkumar@gmail.com>
2022-07-04 20:00:32 +0530

    First Commit

---
+++ src/main/java/com/example/singleshotdetection/SingleshotdetectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.singleshotdetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleshotdetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleshotdetectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/singleshotdetection/SingleshotdetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.singleshotdetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleshotdetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
